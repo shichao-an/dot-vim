d1c447133f624f04515d6577c1b8267e8c56c1d3
Shichao An <shichao.an@nyu.edu>
2014-11-16 23:45:33 -0800

    Update deployment method using submodule

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,7 @@
 
 # Deploy .vimrc and download bundles using git clone
 
-BUNDLE_FILE="$PWD/bundle.txt"
-DOT_VIM_DIR="$HOME/.vim"
-
-cp .vimrc $HOME
-mkdir "$DOT_VIM_DIR"
-rsync -avz .vim/ftplugin "$DOT_VIM_DIR"
-rsync -avz .vim/autoload "$DOT_VIM_DIR"
-mkdir "$DOT_VIM_DIR/bundle"
-
-# Read Git repository URLs from bundle.txt
-cd "$DOT_VIM_DIR/bundle" 
-while read url 
-do
-    git clone $url
-done < $BUNDLE_FILE 
+git submodule update --init --recursive
+echo 'Copying .vim directory to $HOME...'
+cp -a .vim $HOME
+echo 'Please copy .vimrc or .vimrc-osx to $HOME.'
