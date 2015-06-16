1a1a2fa6d5f99da4a7cf7ce94314cb969826e3cd
Shichao An <shichao.an@nyu.edu>
2015-06-16 10:46:01 -0700

    Fixed scripts interpreter files

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Deploy .vimrc, .vim and download bundles using git clone
 
--- pre-commit.sh
+++ pre-commit.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Sync files from ~/.vim and ~/.vimrc before every commit
 rsync -avz --exclude 'bundle' --exclude '.netrwhist' ~/.vim .
